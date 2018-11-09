365492998ba42c080d0625376148cc5cc2adbd7c
Blud7 <paul.riviere.pr@gmail.com>
2018-11-09 02:12:07 +0100

    last commit

--- app/services/search_movie.rb
+++ app/services/search_movie.rb
@@ -6,6 +6,8 @@ class SearchMovie
     @post = k
     @result = []
     Tmdb::Api.Rails.application.credentials.dig(:key_name)
+    # si ça ne marche pas, décommenter la ligne du dessous et la remplacer à celle au-dessus avec sa clé d'API à la place de "key_name" :
+    # Tmdb::Api.key('key_name')
 
   end
 
